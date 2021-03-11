7d87c2c0dfc030a978e6e6cfa43f37e3f6680eb9
ezhiliraja <ezhiliraja@DESKTOP-2267IQ6>
2021-03-11 16:53:10 +0530

    test commit

---
+++ src/test/java/testcase/ezhil_testcase.java
@@ -0,0 +1,36 @@
+package testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import action.Baseclass;
+import pages.CreateJob;
+import pages.Customer;
+import pages.Diary;
+import pages.Estimate;
+import pages.Invoice;
+import pages.LoginPage;
+import pages.NotesandCommunication;
+
+public class ezhil_testcase extends Baseclass {
+	
+	String homepage;
+   
+	
+	
+	@Test(priority = 0)
+	public void login() throws InterruptedException
+	{
+		driver.get("https://dev.commusoft.net/customers");
+	}
+	@Test(priority = 1)
+	public void Addpayment_job() throws InterruptedException
+	{
+		driver.findElement(By.xpath("id=\"customertypecustomer\"")).click();
+	}
+	
+		
+}
+
+
+
