ae0ff4de4818aedc40b86a8dbb22a1df078a6bb0
Karana <Karana@DESKTOP-2KDPHSK>
2021-03-17 17:48:09 +0530

    test2

--- src/test/java/testcase/Karna.java
+++ src/test/java/testcase/Karna.java
@@ -1,10 +1,30 @@
 package testcase;
 
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
 import action.Baseclass;
+import pages.LoginPage;
 
 public class Karna extends Baseclass {
 
+String homepage;
+   
 	
+	
+	@Test(priority = 0)
+	public void login() throws InterruptedException
+	{
+		LoginPage loginpage = PageFactory.initElements(driver, LoginPage.class);
+		loginpage.login(sheet.sheetIN("Login",0, 1),sheet.sheetIN("Login",1, 1),sheet.sheetIN("Login", 2, 1));
+		Thread.sleep(1000);
+		homepage = driver.getCurrentUrl();
+	}
+	@Test(priority = 1)
+	public void Addpayment_job() throws InterruptedException
+	{
+		
+	}
 	
 	
 }
