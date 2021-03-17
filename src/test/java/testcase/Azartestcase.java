9b478a5dffaf45c49a3c1117a07cd49b5048a09c
Mohamed Azarudeen <Mohamed Azarudeen@DESKTOP-UBM9FTJ>
2021-03-17 18:05:10 +0530

    Azar

---
+++ src/test/java/testcase/Azartestcase.java
@@ -0,0 +1,36 @@
+package testcase;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import action.Baseclass;
+import pages.CreateJob;
+import pages.Invoice;
+import pages.LoginPage;
+import pages.NotesandCommunication;
+
+public class Azartestcase extends Baseclass {
+	
+	String homepage;
+   
+	
+	
+	@Test(priority = 0)
+	public void login() throws InterruptedException
+	{
+		LoginPage loginpage = PageFactory.initElements(driver, LoginPage.class);
+		loginpage.login(sheet.sheetIN("Login",0, 1),sheet.sheetIN("Login",1, 1),sheet.sheetIN("Login", 2, 1));
+		Thread.sleep(1000);
+		homepage = driver.getCurrentUrl();
+	}
+	
+	@Test(priority = 1)
+	public void Addpayment_job() throws InterruptedException
+	{
+	}
+	
+		
+
+}
+
+
